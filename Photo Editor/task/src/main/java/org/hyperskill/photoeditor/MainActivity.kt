8b9eb56bef7eb91a6d8a0d8525ff88d3eade0f98
ComfyChair <unknown.entity@gmx.de>
2024-10-18 08:34:08 +0200

    brightness filter implementation with for loops, quite laggy

--- Photo Editor/task/src/main/java/org/hyperskill/photoeditor/MainActivity.kt
+++ Photo Editor/task/src/main/java/org/hyperskill/photoeditor/MainActivity.kt
@@ -3,6 +3,7 @@ package org.hyperskill.photoeditor
 import android.app.Activity
 import android.content.Intent
 import android.graphics.Bitmap
+import android.graphics.BitmapFactory
 import android.graphics.Color
 import android.os.Bundle
 import android.provider.MediaStore
@@ -10,29 +11,45 @@ import android.widget.Button
 import android.widget.ImageView
 import androidx.activity.result.contract.ActivityResultContracts.StartActivityForResult
 import androidx.appcompat.app.AppCompatActivity
+import com.google.android.material.slider.Slider
+import java.io.InputStream
 
 
 class MainActivity : AppCompatActivity() {
 
     private lateinit var currentImage: ImageView
     private lateinit var galleryBtn: Button
+    private lateinit var brightnessSlider: Slider
+    private lateinit var originalBitmap: Bitmap
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         bindViews()
+        setListeners()
 
         //do not change this line
         currentImage.setImageBitmap(createBitmap())
+        originalBitmap = createBitmap()
     }
 
 
     private fun bindViews() {
         currentImage = findViewById(R.id.ivPhoto)
         galleryBtn = findViewById(R.id.btnGallery)
+        brightnessSlider = findViewById(R.id.slBrightness)
+    }
+
+    private fun setListeners() {
         galleryBtn.setOnClickListener {
-            activityResultLauncher.launch(Intent(
-                Intent.ACTION_PICK,
-                MediaStore.Images.Media.EXTERNAL_CONTENT_URI))
+            activityResultLauncher.launch(
+                Intent(
+                    Intent.ACTION_PICK,
+                    MediaStore.Images.Media.EXTERNAL_CONTENT_URI
+                )
+            )
+        }
+        brightnessSlider.addOnChangeListener { slider, value, fromUser ->
+            currentImage.setImageBitmap(applyBrightness(originalBitmap, value))
         }
     }
 
@@ -40,10 +57,37 @@ class MainActivity : AppCompatActivity() {
         registerForActivityResult(StartActivityForResult()) { result ->
             if (result.resultCode == Activity.RESULT_OK) {
                 val photoUri = result.data?.data ?: return@registerForActivityResult
-                // code to update ivPhoto with loaded image
-                currentImage.setImageURI(photoUri)
+                // update ivPhoto with loaded image and get Bitmap to apply filters
+                //currentImage.setImageURI(photoUri) // this would only display the image
+                brightnessSlider.value = 0.0f // reset slider position
+                contentResolver.openInputStream(photoUri).use {
+                    originalBitmap = BitmapFactory.decodeStream(it)
+                }
+                currentImage.setImageBitmap(originalBitmap)
+
             }
         }
+
+    private fun applyBrightness(inBitmap: Bitmap, offset: Float): Bitmap {
+        val outBitmap = inBitmap.copy(Bitmap.Config.RGB_565, true)
+        val height = outBitmap.height
+        val width = outBitmap.width
+        for (y in 0 until height) {
+            for (x in 0 until width) {
+                val R = (Color.red(inBitmap.getPixel(x, y))
+                        + offset.toInt())
+                    .coerceIn(0, 255)
+                val G = (Color.green(inBitmap.getPixel(x, y))
+                        + offset.toInt())
+                    .coerceIn(0, 255)
+                val B = (Color.blue(inBitmap.getPixel(x, y))
+                        + offset.toInt())
+                    .coerceIn(0, 255)
+                outBitmap.setPixel(x, y, Color.rgb(R, G, B))
+            }
+        }
+        return outBitmap
+    }
 
     // do not change this function
     fun createBitmap(): Bitmap {
